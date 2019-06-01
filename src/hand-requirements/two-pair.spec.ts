36560c258bce056a06e60ef97f90425fcd71a587
Alexander Bird <alex@alexanderbird.software>
2019-06-01 10:09:14 -0600

    Add the two pair hand requirement

---
+++ src/hand-requirements/two-pair.spec.ts
@@ -0,0 +1,25 @@
+import { HandScoreCardPart } from '../model';
+import { twoPair } from './two-pair';
+import { club, heart, diamond, spade } from '../deck';
+
+describe('twoPair', () => {
+  it('does not match when there is no pair', () => {
+    const match = twoPair([ club.two, heart.four, diamond.three, spade.seven, club.nine ]);
+    expect(match).toEqual(false);
+  });
+
+  it('does not match when there is one pair', () => {
+    const match = twoPair([ club.two, heart.seven, diamond.three, spade.seven, club.nine ]);
+    expect(match).toEqual(false);
+  });
+
+  it('matches the first card of each pair as the key cards when there are two pairs', () => {
+    const match = twoPair([ club.two, heart.seven, diamond.nine, spade.seven, club.nine ]);
+    expect((match as HandScoreCardPart).keyCards).toEqual([ diamond.nine, heart.seven ]);
+  });
+
+  it('matches the fifth card as the kicker when there are two pairs', () => {
+    const match = twoPair([ club.two, heart.seven, diamond.nine, spade.seven, club.nine ]);
+    expect((match as HandScoreCardPart).kicker).toEqual([ club.two ]);
+  });
+});
---
+++ src/hand-requirements/two-pair.ts
@@ -0,0 +1,4 @@
+import { pair } from './pair';
+import { union } from './union';
+
+export const twoPair = union(pair, pair);
