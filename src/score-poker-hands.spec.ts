b69fbc32a323c34802d87cf8c0bb8475e14925d6
Alexander Bird <alex@alexanderbird.software>
2019-06-02 06:52:17 -0600

    Move fixtures for integration test to top level directory
    Also slightly improve the test message for those integration tests.
    
    The examples for the integration tests serve as a good documentation for
    what scenarios I have covered. It would probably be helfpul for
    a reviewer to see that in the top level directory. Skimming through it
    should give them a quick overview of what this implementation does (and
    doesn't?) do.

--- src/score-poker-hands.spec.ts
+++ src/score-poker-hands.spec.ts
@@ -3,16 +3,16 @@ import { join } from 'path';
 import { scorePokerHands } from './score-poker-hands';
 
 describe('scorePokerHands', () => {
-  const fixturePath = join(__dirname, './score-poker-hands.spec.fixtures');
+  const fixturePath = 'examples';
   const fixtures = readdirSync(fixturePath);
   fixtures.forEach(fixtureFileName => {
     const fixtureNameMatch = fixtureFileName.match(/(.*)\.txt$/);
     if(!fixtureNameMatch) return;
     const fixtureName = fixtureNameMatch[1];
     const fixture = readFileSync(join(fixturePath, fixtureFileName)).toString();
-    it(`correctly scores games with a ${fixtureName.replace('-', ' ')}`, () => {
+    it(`correctly scores the "${fixtureName.replace('-', ' ')}" example game`, () => {
       const [ input, output ] = fixture.split('\n\n---\n\n');
-      expect(scorePokerHands(input)).toEqual(output.trim());
+      expect(scorePokerHands(input)).toEqual(output);
     });
   });
 });
--- src/score-poker-hands.ts
+++ src/score-poker-hands.ts
@@ -1,5 +1,6 @@
 export function scorePokerHands(input: string): string {
   return `1 Becky Straight Ace
 2 Sam Two Pair Ace King
-3 John Pair 7`;
+3 John Pair 7
+`;
 }
