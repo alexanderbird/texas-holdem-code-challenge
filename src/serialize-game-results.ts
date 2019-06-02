c36eb3eb89cebbdb25dff5a5a18d9a4e793dd3c9
Alexander Bird <alex@alexanderbird.software>
2019-06-02 10:10:42 -0600

    Update serializeGameResults to properly report ties

--- src/serialize-game-results.ts
+++ src/serialize-game-results.ts
@@ -2,32 +2,52 @@ import { GameResults, PlayerHandClassification } from './model';
 
 function serializeOneLine(
   { player, handClassification }: PlayerHandClassification,
-  i: number,
-  numberOfKickers: number
+  rankString: string,
+  numberOfRelevantKickers: number
 ): string {
-  return `${i + 1} ${player.name} ${handClassification.serializeForHumans(numberOfKickers)}\n`;
+  return `${rankString} ${player.name} ${handClassification.serializeForHumans(numberOfRelevantKickers)}\n`;
+}
+
+interface KickerSummary {
+  numberOfRelevantKickers: number;
+  isTiedWithNext: boolean;
+}
+
+function compareWithSiblings(gameResults: GameResults, i: number): KickerSummary {
+  const thisOne = gameResults[i];
+  const before = gameResults[i - 1];
+  const after = gameResults[i + 1];
+  let numberOfKickersVSBefore = 0;
+  let numberOfKickersVSAfter = 0;
+  if(before) {
+    numberOfKickersVSBefore = thisOne.handClassification.howManyKickerCardsAreRelevant(
+      before.handClassification
+    );
+  }
+  if(after) {
+    numberOfKickersVSAfter = thisOne.handClassification.howManyKickerCardsAreRelevant(
+      after.handClassification
+    );
+  }
+  return {
+    numberOfRelevantKickers: Math.max(numberOfKickersVSBefore, numberOfKickersVSAfter),
+    isTiedWithNext: numberOfKickersVSAfter === Infinity,
+  };
 }
 
 export function serializeGameResults(gameResults: GameResults): string {
   let result = '';
+  const unresolvedRankings: ((rankString: string) => string)[] = [];
   for(let i = 0; i < gameResults.length; i++) {
-    const thisOne = gameResults[i];
-    const before = gameResults[i - 1];
-    const after = gameResults[i + 1];
-    let numberOfKickersVSBefore = 0;
-    let numberOfKickersVSAfter = 0;
-    if(before) {
-      numberOfKickersVSBefore = thisOne.handClassification.howManyKickerCardsAreRelevant(
-        before.handClassification
-      );
+    const { isTiedWithNext, numberOfRelevantKickers } = compareWithSiblings(gameResults, i);
+    unresolvedRankings.push((rankString: string) => serializeOneLine(gameResults[i], rankString, numberOfRelevantKickers));
+    if(!isTiedWithNext) {
+      const rankString = (i + 1).toString();
+      unresolvedRankings.forEach(method => {
+        result += method(rankString);
+      });
+      unresolvedRankings.length = 0;
     }
-    if(after) {
-      numberOfKickersVSAfter = thisOne.handClassification.howManyKickerCardsAreRelevant(
-        after.handClassification
-      );
-    }
-    const numberOfKickers = Math.max(numberOfKickersVSBefore, numberOfKickersVSAfter);
-    result += serializeOneLine(gameResults[i], i, numberOfKickers);
   }
   return result;
 }
