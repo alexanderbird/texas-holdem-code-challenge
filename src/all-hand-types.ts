cffa8cb15791c8f3855db299c6c56da0833e5677
Alexander Bird <alex@alexanderbird.software>
2019-06-02 09:18:34 -0600

    Add hand types for pair, two pair, and straight
    Which meant I had to update the high cards examples so they didn't
    include any of those three hands.

--- src/all-hand-types.ts
+++ src/all-hand-types.ts
@@ -1,6 +1,13 @@
 import { HandType } from './hand-type';
 import { highCard } from './hand-requirements/high-card';
+import { pair } from './hand-requirements/pair';
+import { twoPair } from './hand-requirements/two-pair';
+import { straight } from './hand-requirements/straight';
 
+let rank = 1;
 export const allHandTypes: HandType[] = [
-  new HandType('High Card', 1, highCard),
+  new HandType('High Card' , rank++ , highCard) ,
+  new HandType('Pair'      , rank++ , pair)     ,
+  new HandType('Two Pair'  , rank++ , twoPair)  ,
+  new HandType('Straight'  , rank++ , straight) ,
 ]
