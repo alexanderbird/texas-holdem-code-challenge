483f9b87aba984d52d1ae50d1826b24c9f3fdbcc
Alexander Bird <alex@alexanderbird.software>
2019-06-03 21:52:17 -0600

    Update the cli help text to be a bit more clear

--- src/cli-help-text.ts
+++ src/cli-help-text.ts
@@ -28,7 +28,12 @@ export const greeting = `
       / __  / (_) | | (_| |  __/ | | | | |
       \\/ /_/ \\___/|_|\\__,_|\\___|_| |_| |_|
 
-Describe the game board. For an example, run this again with the --example flag.
+Describe the game board.
+
+For an example, run \`npm start -- --example\`
+To run this without the ascii art fluff, pipe your input in directly. For example:
+
+  npm start < examples/every-hand-type.txt
 
 Ctrl+D to score the game.
 Ctrl+C to exit.
